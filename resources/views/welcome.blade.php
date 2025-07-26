4244309f0ee45c29c6340d5b0d195544e0cc1584
Karol <39806518+whichmann@users.noreply.github.com>
2025-07-26 22:20:53 +0200

    to blade

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>spaceblog</title>
+</head>
+<body>
+    <h1>Welcome to spaceblog</h1>
+</body>
+</html>
---
+++ storage/framework/views/c5f268f29e609735f5b07ae72315a51c.php
@@ -0,0 +1,11 @@
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>spaceblog</title>
+</head>
+<body>
+    <h1>Welcome to spaceblog</h1>
+</body>
+</html>
+<?php /**PATH /Users/karolwichmann/Downloads/spaceblog-ws/spaceblog-ws/resources/views/welcome.blade.php ENDPATH**/ ?>
